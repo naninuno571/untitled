8456229d89c8d21e02835222d278aa7cb25347cf
SogabeTaiyou <241034@std.hi-joho.ac.jp>
2025-05-16 12:17:51 +0900

    課題1～５追加

---
+++ src/review4.java
@@ -0,0 +1,13 @@
+public class review4 {
+    public static void main(String[] args) {
+        int number =5;
+        String str = "88"+"88";
+        final double PI = 3.141592;
+        double pi = 3.14;
+        long a=1000000000;
+        boolean flg =true;
+        byte b=35;
+        int num= 1845;
+        char c='a';
+    }
+}
