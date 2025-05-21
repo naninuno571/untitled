9692d57633e955bb5e48f9146fe8d8ef7a1abf6e
SogabeTaiyou <241034@std.hi-joho.ac.jp>
2025-05-21 10:05:36 +0900

    課題11の追加

---
+++ src/review11.java
@@ -0,0 +1,10 @@
+public class review11 {
+    public static void main(String[] args) {
+        int num = 1000;
+        if(!(0 == num % 10)){
+            System.out.println("この数は10で割り切れない値です");
+        } else {
+            System.out.println("この数は10で割り切れる値です");
+        }
+    }
+}
