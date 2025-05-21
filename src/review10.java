a52f7b0641555eec3a8487ab153b9c01f2e2a3a4
SogabeTaiyou <241034@std.hi-joho.ac.jp>
2025-05-21 10:01:33 +0900

    課題10の追加

---
+++ src/review10.java
@@ -0,0 +1,10 @@
+public class review10 {
+    public static void main(String[] args) {
+        int num = 10;
+        if(num>0){
+            System.out.println("この数は正の数です");
+        } else {
+            System.out.println("この数は正の数ではありません");
+        }
+    }
+}
