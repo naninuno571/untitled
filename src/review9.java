f5fad4dc4799001312b33af971a44e5c7f7e5f6d
SogabeTaiyou <241034@std.hi-joho.ac.jp>
2025-05-21 09:57:24 +0900

    課題8の追加

---
+++ src/review9.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class review9 {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        System.out.print("名前を入力してください＞");
+        String inputName = br.readLine();
+        System.out.print("年齢を入力してください＞");
+        int age = Integer.parseInt(br.readLine());
+
+        System.out.print(age+"歳の"+inputName+"さん。\nこんにちわ。");
+
+    }
+}
