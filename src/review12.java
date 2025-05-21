659c7c39f44567eaca2f045cf3769b873cca356f
SogabeTaiyou <241034@std.hi-joho.ac.jp>
2025-05-21 10:20:43 +0900

    課題12の追加

---
+++ src/review12.java
@@ -0,0 +1,14 @@
+public class review12 {
+    public static void main(String[] args) {
+        int age = -1;
+        if (age >= 0 && age<=17) {
+            System.out.println("未成年");
+        } else if (age >= 18 && age<=64) {
+            System.out.println("成人");
+        } else if (age >= 65) {
+            System.out.println("高齢者 ");
+        } else{
+            System.out.println("無効な年齢です");
+        }
+    }
+}
