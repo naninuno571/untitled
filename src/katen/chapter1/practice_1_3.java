f936504ef93ecfd9fe2f22896b7363bcdc1f89e7
SogabeTaiyou <241034@std.hi-joho.ac.jp>
2025-06-04 09:28:11 +0900

    追加課題１の追加

---
+++ src/katen/chapter1/practice_1_3.java
@@ -0,0 +1,11 @@
+package katen.chapter1;
+
+public class practice_1_3 {
+    public static void main(String[] args) {
+        boolean a=true;
+        char b='駆';
+        double c=3.14;
+        long d=314159265853979l;
+        String e="ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    }
+}
